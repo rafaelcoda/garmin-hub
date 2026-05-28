ebfbe98ab60fb867344e1df3a53ec50d58a0542f
rafaelcoda <50381298+rafaelcoda@users.noreply.github.com>
2026-05-28 15:20:42 -0300

    fix: types/oauth-1.0a.d.ts

---
+++ types/oauth-1.0a.d.ts
@@ -0,0 +1,30 @@
+declare module 'oauth-1.0a' {
+  interface Token {
+    key: string
+    secret: string
+  }
+
+  interface RequestData {
+    url: string
+    method: string
+    data?: Record<string, unknown>
+  }
+
+  interface OAuthOptions {
+    consumer: Token
+    signature_method: string
+    hash_function: (baseString: string, key: string) => string
+  }
+
+  interface OAuthHeader {
+    Authorization: string
+  }
+
+  class OAuth {
+    constructor(options: OAuthOptions)
+    authorize(requestData: RequestData, token?: Token): Record<string, string>
+    toHeader(oauthData: Record<string, string>): OAuthHeader
+  }
+
+  export = OAuth
+}
